f4a0e2415937ae8deb86a075d3d8d2fdb972b4df
Aleksey <aleksey.kalyuzhnyi@gmail.com>
2020-04-21 00:53:09 +0300

    added support ie

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,6 +1,32 @@
+import "@babel/polyfill";
 
 import '../../../node_modules/svgxuse/svgxuse';
 import Slide from './slider'
+
+// ParentNode.append polyfill
+(function (arr) {
+    arr.forEach(function (item) {
+      if (item.hasOwnProperty('append')) {
+        return;
+      }
+      Object.defineProperty(item, 'append', {
+        configurable: true,
+        enumerable: true,
+        writable: true,
+        value: function append() {
+          var argArr = Array.prototype.slice.call(arguments),
+            docFrag = document.createDocumentFragment();
+          
+          argArr.forEach(function (argItem) {
+            var isNode = argItem instanceof Node;
+            docFrag.appendChild(isNode ? argItem : document.createTextNode(String(argItem)));
+          });
+          
+          this.appendChild(docFrag);
+        }
+      });
+    });
+  })([Element.prototype, Document.prototype, DocumentFragment.prototype]);
 
 (function() {
     const headerSlider = new Slide({
